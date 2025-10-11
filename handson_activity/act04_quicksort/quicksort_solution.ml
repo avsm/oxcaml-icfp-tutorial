bbb271e0d673effb269cdc6ad3e89a1ddf5a6462
Diana Kalinichenko <dkalinichenko@janestreet.com>
2025-10-11 10:33:22 -0400

    solve quicksort

--- handson_activity/act04_quicksort/quicksort_solution.ml
+++ handson_activity/act04_quicksort/quicksort_solution.ml
@@ -1,4 +1,5 @@
-module Capsule = Portable.Capsule.Expert
+open! Await
+module Scheduler = Parallel_scheduler_work_stealing
 module Par_array = Parallel.Arrays.Array
 module Slice = Par_array.Slice
 
@@ -25,7 +26,7 @@ let partition slice =
   let rec quicksort par slice =
     if 1 < Slice.length slice then ( 
       let pivot = partition slice in 
-      let (), () = 
+      let #((), ()) = 
         Slice.fork_join2 par ~pivot slice 
           (fun par left -> quicksort par left)
           (fun par right -> quicksort par right) 
@@ -33,14 +34,15 @@ let partition slice =
     ) 
 
   let quicksort ~scheduler ~mutex array =
-    let monitor = Parallel.Monitor.create_root () in
-    Parallel_scheduler_work_stealing.schedule scheduler ~monitor ~f:(fun parallel ->
-      Capsule.Mutex.with_lock mutex ~f:(fun password ->
-        Capsule.Data.iter array ~password ~f:(fun array ->
-          let array = Par_array.of_array array in
-          quicksort parallel (Slice.slice array) [@nontail])
-        [@nontail])
+    Scheduler.parallel scheduler ~f:(fun parallel ->
+      Await_blocking.with_await Terminator.never ~f:(fun wait ->
+      Capsule.Mutex.with_lock wait mutex ~f:(fun access ->
+        (Capsule.Data.unwrap ~access array
+        |> Par_array.of_array
+        |> Slice.slice
+        |> quicksort parallel)[@nontail])
       [@nontail])
+    [@nontail])
 
 (* Example usage and tests *)
 let () =
@@ -49,15 +51,15 @@ let () =
   in
   let n = 10000 in
 
-  let (P key) = Capsule.create () in
-  let mutex = Capsule.Mutex.create key in
+  let (P mutex) = Capsule.Mutex.create () in
   let array =
     Capsule.Data.create (fun () -> Array.init n (fun _ -> Random.int n))
   in
   quicksort ~scheduler ~mutex array ;
-  Capsule.Mutex.with_lock mutex ~f:(fun password ->
-    Capsule.Data.iter array ~password ~f:(fun array ->
-      Array.iter (Printf.printf "%d ") array ;
-      for i = 0 to n - 2 do 
-        assert (array.(i) <= array.(i + 1))
-      done ) )
+  Await_blocking.with_await Terminator.never ~f:(fun wait ->
+  Capsule.Mutex.with_lock wait mutex ~f:(fun access ->
+    let array = Capsule.Data.unwrap ~access array in
+    Array.iter (Printf.printf "%d ") array ;
+    for i = 0 to n - 2 do 
+      assert (array.(i) <= array.(i + 1))
+    done))
